eb4e3233c08c889540a296b5d27ed89e0597fb0d
peskyji <peskyji.com>
2023-09-19 20:48:51 +0530

    App updated Model added

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,32 +1,59 @@
-from unittest.mock import Base
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
+from nltk.stem import PorterStemmer
+import pandas as pd
+import re
+import json
 
 # create an object of FastAPI
 app = FastAPI()
 
 # create objects to receive input from client
 class Input(BaseModel):
-    num1 : float
-    num2 : float
-    num3 : float = 0.1
-    text : str = None
+    reviews : str = None
 
 # create route for index page
 @app.get("/")
 def homepage():
-    print("Hi this is index page")
+    # print("Hi this is index page")
     return {'a':'b'}
 
 # create another route for calculation
-@app.post("/calculate")
-def calculate(data:Input):
-    my_dict={}
-    if data.num3 != 0.1:
-        return {'sum':data.num1+data.num2+data.num3}
-    else:
-        return {'sum':data.num1+data.num2, "num3":data.num3, "text":data.text}
 
+def preprocessing_task(review):
+    ps = PorterStemmer()
+    review = re.sub(r'[^A-Za-z\s]', '', review)
+    # print("Special Characters removed from review")
+    review = re.sub(r' +', ' ', review)
+    # print("extra spaces removed")
+    review = review.lower()
+    # print("Word Stemming stated...")
+    review = " ".join([ps.stem(word) for word in review.split()])
+    # print("Stemming Completed")
+    return review
+
+@app.post("/predict")
+def prediction(data:Input):
+    tfidf = pickle.load(open('Models/tfidf.pkl', 'rb'))
+    lr = pickle.load(open('Models/lr.pkl', 'rb'))
+    xgb = pickle.load(open('Models/xgb.pkl', 'rb'))
+    cnb = pickle.load(open('Models/cnb.pkl', 'rb'))
+
+    review = preprocessing_task(data.reviews)
+    X = tfidf.transform(pd.Series(review))
+    # print("vector created")
+
+    lr_prob = lr.predict_proba(X)[0]
+    xgb_prob = xgb.predict_proba(X)[0]
+    cnb_prob = cnb.predict_proba(X)[0]
+
+    prob = {
+                'lr':[float(lr_prob[0]), float(lr_prob[1])], 
+                'xgb':[float(xgb_prob[0]), float(xgb_prob[1])],
+                'cnb':[float(cnb_prob[0]), float(cnb_prob[1])]
+            }
+    return json.dumps(prob)
 
 
 
