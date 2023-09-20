5cf19694f1882138eb51278fa04335da715aef7c
peskyji <peskyji.com>
2023-09-20 20:25:40 +0530

    code updated

--- fastapi_app.py
+++ fastapi_app.py
@@ -11,7 +11,7 @@ import joblib
 app = FastAPI()
 
 if __name__ == "__main__":
-    run(app, timeout_keep_alive=30)
+    run(app, timeout_keep_alive=60)
 
 # create objects to receive input from client
 class Input(BaseModel):
